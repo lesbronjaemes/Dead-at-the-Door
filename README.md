637c2ac602e24e29d10a1b06c32ea5bd2056b61e
lesbronjaemes <colby.welliver@rbe.sk.ca>
2024-03-12 10:36:43 -0600

    Version 1.1

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 description coming soon :)
 
 # Changelog
+### Version 1.1
+```
+[+] Added functional commands
+```
 ### Version 1.0
 ```
 [+] Initialized the project
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+from os import system
+
 levels = {
-    "start": {
-        "location": "Home Base | Entrance",
+    "abandonedBuildingEntrance": {
+        "location": "Abandoned Building | Entrance",
         "firstTime": [
             True,
             "When the undead first attacked, we weren't ready.\n"\
@@ -12,14 +14,39 @@ levels = {
             "A huge horde of the living dead are coming this way,\n"\
             "they'll reach us by sunset. I have to lock this place\n"\
             "up before then, and maybe, just maybe, i'll be able to\n"\
-            "live another day."
+            "live another day.\n\n"\
+            "You can use help for a list of commands! Stay alive!"
+        ],
+        "states": {
+            "open": "A sturdy, open wide door looks down over you.",
+            "closed": "A sturdy, closed door looks down over you.",
+            "locked": "A sturdy, locked door looks down over you.",
+            "current": "open"
+        },
+        "movement": {
+            "abandonedBuildingWindow": {
+                "action": "west",
+                "time": 0
+            },
+            "abandonedBuildingOutside": {
+                "action": "east",
+                "time": 0,
+                "condition": "open",
+                "unsuccesful": "The door is shut. That's not going to work buster."
+            }
+        },
+        "interactions": [
+            "door"
         ]
+    },
+    "abandonedBuildingWindow": {
+        "location": "Abandoned Building | Window"
     }
 }
 
 class level:
     def __init__(self):
-        self.scene = "start"
+        self.scene = "abandonedBuildingEntrance"
         self.time = 480
     
     def convertTime(self):
@@ -33,12 +60,51 @@ class level:
     
     def hud(self):
         current = levels[self.scene]
-        print(current["location"])
-        print(self.convertTime())
-        print("_____________________________________________________________________")
+        inputCheck = True
+
+        print("Location: " + current["location"])
+        print("Time Left: " + self.convertTime())
+        print("_____________________________________________________________________\n")
 
         if current["firstTime"][0]:
-            print(current["firstTime"][1])
+            print(current["firstTime"][1] + "\n")
+            current["firstTime"][0] = False
         
-test = level()
-test.hud()
+        print(current["states"][current["states"]["current"]] + "\n")
+
+        action = input("Action: ").lower().split()
+
+        system("cls")
+
+        if action != "":
+            if action[0] == "use"[:len(action[0])]:
+                print(action[1])
+                inputCheck = False
+            elif action[0] == "interact"[:len(action[0])]:
+                if action[1] == "door"[:len(action[1])]:
+                    if current["states"]["current"] != "locked":
+                        if current["states"]["current"] == "open":
+                            current["states"]["current"] = "closed"
+                        else:
+                            current["states"]["current"] = "open"
+                
+                    inputCheck = False
+                else:
+                    for i in current["interactions"]:
+                        if action[1] == i[:len(action[1])]:
+                            pass
+            else:
+                for i, v in current["movement"].items():
+                    if action[0] == v["action"][:len(action[0])]:
+                        #self.scene = i
+                        print(i)
+                        inputCheck = False
+        
+        if inputCheck:
+            print("Invalid Input! Try Again!")
+
+        
+game = level()
+
+while True:
+    game.hud()
